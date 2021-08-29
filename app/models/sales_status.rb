68c139652b7049efa6868d77910d30ac88aca9cc
takumi-kani <hxjtp796@gmail.com>
2021-08-29 18:44:43 +0900

    アクティブハッシュにidを追記

--- app/models/sales_status.rb
+++ app/models/sales_status.rb
@@ -1,7 +1,7 @@
 class SalesStatus < ActiveHash::Base
   self.data = [
-    {name: '新品・未使用'}, {name: '未使用に近い'},
-    {name: '目立った傷や汚れなし'}, {name: 'やや傷や汚れあり'}, {name: '傷や汚れあり'},
-    {name: '全体的に状態が悪い'}
+    {id: 1, name: '新品・未使用'}, {id: 2, name: '未使用に近い'},
+    {id: 3, name: '目立った傷や汚れなし'}, {id: 4, name: 'やや傷や汚れあり'}, {id: 5, name: '傷や汚れあり'},
+    {id: 6, name: '全体的に状態が悪い'}
   ]
 end
