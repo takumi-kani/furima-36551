d7153069c47ed40bc30aba8acb4ae3848a6d907e
takumi-kani <hxjtp796@gmail.com>
2021-08-29 18:37:02 +0900

    デバッグ用console.logの削除

--- app/javascript/packs/tax.js
+++ app/javascript/packs/tax.js
@@ -3,16 +3,12 @@ window.addEventListener("load",()=>{
   const inputPrice = document.getElementById("item-price");
   inputPrice.addEventListener("input", () => {
     const inputValue = inputPrice.value;
-    console.log(inputValue);
 
     const addTax = document.getElementById("add-tax-price")
     addTax.innerHTML = (Math.floor(inputValue * 0.1));
-     console.log(addTax);
 
     const profitPrice = document.getElementById("profit")
       const price_result = inputValue * 0.1
-      console.log(price_result)
       profitPrice.innerHTML = (Math.ceil(inputValue - price_result));
-        console.log(profitPrice);
   })
 })
